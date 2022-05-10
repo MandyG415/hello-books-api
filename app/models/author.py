08ecc821f2a66b41ef850462ab1e0b1376faa8ac
MandyG415 <missmandygao@gmail.com>
2022-05-09 21:21:37 -0700

    Added authors to author table

--- app/models/author.py
+++ app/models/author.py
@@ -1,5 +1,5 @@
 from app import db
 
 class Author(db.Model):
-  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  name = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String)
